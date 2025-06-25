7cb69f97f119b76253fd210444bfe74cdaa57961
deepak-gowda-narayana <deepak.narayana@intel.com>
2025-06-25 19:48:42 +0000

    update

--- optimum/habana/transformers/training_args.py
+++ optimum/habana/transformers/training_args.py
@@ -38,7 +38,6 @@ from transformers.trainer_utils import (
     SchedulerType,
 )
 from transformers.training_args import (
-    _VALID_DICT_FIELDS,
     OptimizerNames,
     ParallelMode,
     TrainingArguments,
@@ -436,7 +435,7 @@ class GaudiTrainingArguments(TrainingArguments):
             )
 
         # Parse in args that could be `dict` sent in from the CLI as a string
-        for field in _VALID_DICT_FIELDS:
+        for field in TrainingArguments._VALID_DICT_FIELDS:
             passed_value = getattr(self, field)
             # We only want to do this if the str starts with a bracket to indiciate a `dict`
             # else its likely a filename if supported
